d3eb6cfbc864917cc751cd8f3764306ba3bb9a50
Simon <110613416+ranopashec@users.noreply.github.com>
2024-04-30 09:24:20 +0300

    Update optimization.hpp

--- optimization.hpp
+++ optimization.hpp
@@ -1,9 +1,14 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
+
 #pragma GCC optimize ('03', 'unroll-loops')
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    return 0;
-}
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
